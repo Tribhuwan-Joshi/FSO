6b5c9e0ebdb6349921ccb185b269248bf7b87d66
tjsm <mathincosmos@gmail.com>
2024-05-24 16:28:49 +0530

    finish unicafe task

--- Part1/part1_1.5ex/src/App.jsx
+++ Part1/part1_1.5ex/src/App.jsx
@@ -1,55 +1,52 @@
+import { useState } from "react";
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+
+  const [total, setTotal] = useState(0);
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat("L"));
+    setLeft((prev) => prev + 1);
+    const updatedLeft = left + 1;
+    setTotal(updatedLeft + right);
   };
+
+  const IncrementBy = () => () => setAll(allClicks.concat("LOL"));
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat("R"));
+    setRight((right) => right + 1);
+    const updatedRight = right + 1;
+    setTotal(updatedRight + left);
+    setTotal(left + updatedRight);
+  };
+
+  const Button = ({ handleClick, text }) => (
+    <button onClick={handleClick}>{text}</button>
+  );
+
   return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Footer course={course} />
+      {left}
+      <Button handleClick={handleLeftClick} text="left" />
+      <Button handleClick={handleRightClick} text="right" />
+      <button onClick={IncrementBy()}>Add LOL to stack</button>
+
+      {right}
+      <History allClicks={allClicks} />
+
+      <p>total {total}</p>
     </div>
   );
 };
 
-const Header = (props) => {
-  return <h1>{props.course.name}</h1>;
-};
-
-const Content = (props) => {
-  const parts = props.course.parts;
-  return (
-    <>
-      <Part name={parts[0].name} exercises={parts[0].exercises} />
-      <Part name={parts[1].name} exercises={parts[1].exercises} />
-      <Part name={parts[2].name} exercises={parts[2].exercises} />
-    </>
+const History = ({ allClicks }) =>
+  allClicks.length ? (
+    <p>Clicked history - {allClicks.join(" ")}</p>
+  ) : (
+    <p>App is used by pressing Button</p>
   );
-};
 
-const Footer = (props) => {
-  const total = props.course.parts.reduce((f, s) => f + s.exercises, 0);
-  return <p>Number of exercise {total}</p>;
-};
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.name} {props.exercises}
-    </p>
-  );
-};
 export default App;
