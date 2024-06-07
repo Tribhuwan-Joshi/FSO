7cc64b5266eb26284d0c0b7e03604af1edb8bd6c
tjsm <mathincosmos@gmail.com>
2024-06-07 10:55:06 +0530

    --no-edit

---
+++ temp/node-intro/index.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+  },
+];
+
+app.get("/", (req, res) => res.send("<h1>Hello World!</h1>"));
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const note = notes.find((n) => n.id === id);
+  if (note) res.json(note);
+  else res.status(404).end("Note not found");
+});
+
+const generateId = () => {
+  const maxId =
+    notes.length > 0
+      ? Math.max.apply(
+          null,
+          notes.map((n) => n.id)
+        )
+      : 0;
+
+  const noteId = maxId + 1;
+  return noteId;
+};
+
+app.post("/api/notes", (req, res) => {
+  const body = req.body;
+  if (!body) {
+    return res.status(400).json({ error: "Content is missing" });
+  }
+
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+  res.json(note);
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const note = notes.find((n) => n.id === id);
+  if (note) {
+    notes = notes.filter((n) => n.id != id);
+    res.status(204).end();
+  }
+  res.status(404).end();
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
