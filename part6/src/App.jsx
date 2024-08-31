70ebe8e92234b3e4bd166d898f947cf64c001c3e
tjsm <mathincosmos@gmail.com>
2024-08-31 17:50:06 +0530

    use react-query

--- part6/src/App.jsx
+++ part6/src/App.jsx
@@ -1,21 +1,69 @@
-import { useEffect } from "react";
-import NewNote from "./components/newNote";
-import Notes from "./components/Notes";
-import VisibilityFilter from "./components/VisibilityFilter";
-import { useDispatch } from "react-redux";
-import { initializeNotes } from "./reducers/noteReducer";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { createNote, getNotes, updateNote } from "../request";
 
 const App = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(initializeNotes());
-  }, []);
+  const queryClient = useQueryClient();
+  //mutation for mutating data on the server
+  const newNoteMutation = useMutation({
+    // it gets the parameter of newNoteMutation
+    mutationFn: createNote,
+    onSuccess: (newNote) => {
+      // queryClient.invalidateQueries({ queryKey: ["notes"] }); // invalidate query
+      // rather than invalidating query, we can manually update the state
+      const notes = queryClient.getQueryData(["notes"]); // get current state
+      queryClient.setQueryData(["notes"], notes.concat(newNote)); // concat to it - IMMUTABLE
+    },
+  });
+
+  const updateNoteMutation = useMutation({
+    mutationFn: updateNote,
+    onSuccess: (updatedNote) => {
+      // queryClient.invalidateQueries({ queryKey: ["notes"] });
+      const notes = queryClient.getQueryData(["notes"]);
+      queryClient.setQueryData(
+        ["notes"],
+        notes.map((n) => (n.id == updatedNote.id ? updatedNote : n))
+      );
+    },
+  });
+
+  const addNote = async (event) => {
+    event.preventDefault();
+    const content = event.target.note.value;
+    event.target.note.value = "";
+    newNoteMutation.mutate({ content, important: false });
+  };
+
+  const toggleImportance = (note) => {
+    updateNoteMutation.mutate({ ...note, important: !note.important });
+  };
+
+  const result = useQuery({
+    queryKey: ["notes"],
+    queryFn: getNotes,
+    refetchOnWindowFocus: false,
+  });
+
+  console.log(JSON.parse(JSON.stringify(result)));
+
+  if (result.isLoading) {
+    return <div>Loading data...</div>;
+  }
+  const notes = result.data;
+
   return (
     <div>
-      <NewNote />
-      <VisibilityFilter />
-      <h3>Notes</h3>
-      <Notes />
+      <h2>Notes app</h2>
+      <form onSubmit={addNote}>
+        <input name="note" />
+        <button type="submit">add</button>
+      </form>
+      {notes.map((note) => (
+        <li key={note.id} onClick={() => toggleImportance(note)}>
+          {note.content}
+          <strong> {note.important ? "important" : ""}</strong>
+        </li>
+      ))}
     </div>
   );
 };
--- part6/src/main.jsx
+++ part6/src/main.jsx
@@ -1,22 +1,12 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
-import noteReducer from "./reducers/noteReducer";
-import filterReducer from "./reducers/filterReducer.js";
-import { StrictMode } from "react";
-
-const store = configureStore({
-  reducer: { notes: noteReducer, filter: filterReducer },
-});
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 //action creator
 const root = createRoot(document.getElementById("root"));
+const queryClient = new QueryClient();
 
 root.render(
-  <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
 );
